de4cdb87691041f19c922b61b69cb28bba4d38c1
afaulkner420 <afaulkner420@gmail.com>
2018-03-18 21:59:48 +0000

    Add configs for fingerprint

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,7 +1,13 @@
 # Fingerprint
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/fingerprint/sw_config.xml:system/etc/sw_config.xml \
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 PRODUCT_PACKAGES += \
     fingerprintd \
     fingerprint.mt6763
+
+
