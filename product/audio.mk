55e111495b6e53c3c843b34e45b984cb78b1e9e1
afaulkner420 <afaulkner420@gmail.com>
2018-04-24 11:46:08 +0100

    Update audio.mk

--- product/audio.mk
+++ product/audio.mk
@@ -1,6 +1,5 @@
 # Audio config
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(DEVICE_PATH)/configs/audio/audio_device.xml:system/etc/audio_device.xml \
     $(DEVICE_PATH)/configs/audio/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
     $(DEVICE_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
