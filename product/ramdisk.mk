68b9c7c642e52c1535be979f043c5eb9e1b16c77
afaulkner420 <afaulkner420@gmail.com>
2018-04-29 17:29:36 +0100

    Woops!

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,14 +1,9 @@
 # device rootdir
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
-    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6763:root/fstab.mt6763 \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6763.rc:root/init.mt6763.rc \
     $(LOCAL_PATH)/rootdir/init.mt6763.usb.rc:root/init.mt6763.usb.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6763.rc:root/ueventd.mt6763.rc \
     $(LOCAL_PATH)/rootdir/init.recovery.mt6763.rc:root/init.recovery.mt6763.rc \
     $(LOCAL_PATH)/rootdir/init.aee.rc:root/init.aee.rc \
@@ -18,9 +13,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.fingerprint.rc:root/init.fingerprint.rc \
     $(LOCAL_PATH)/rootdir/init.sensor.rc:root/init.sensor.rc \
     $(LOCAL_PATH)/rootdir/init.trustkernel.rc:root/init.trustkernel.rc \
-    $(LOCAL_PATH)/rootdir/factory_init.connectivity.rc:root/factory_init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/init.preload.rc:root/init.preload.rc \
     $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/rootdir/meta_init.connectivity.rc:root/meta_init.connectivity.rc \
     $(LOCAL_PATH)/rootdir/multi_init.rc:root/multi_init.rc
